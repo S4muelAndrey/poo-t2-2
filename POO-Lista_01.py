574f0ec80900fd05214efd7b0e56c0e8f12bc298
S4muelAndrey <161077886+S4muelAndrey@users.noreply.github.com>
2025-05-03 13:13:16 -0300

    Update POO-Lista_01.py

--- POO-Lista_01.py
+++ POO-Lista_01.py
@@ -15,12 +15,7 @@ class Circulo():
     def calcular_circunferencia(self):
         self.circunferecia = 2*math.pi * self.raio
         print(self.circunferecia)
-if escolha == 1:
-    x = Circulo()
-    x.raio = int(input("raio: "))
-    x.calcular_area()
-    x.calcular_circunferencia()
-
+        
 class Viagem():
     def __init__(self):
         self.km = 0
@@ -32,11 +27,6 @@ class Viagem():
         print("velocidade =", self.velocidade, "km/h")
         
 
-if escolha == 2:
-    y = Viagem()
-    y.km = int(input("km andados: "))
-    y.horas = input("tempo em formato hh:mm:\n")
-    y.calc_velocidade()
 
 class Conta_Bancaria():
     def __init__(self):
@@ -55,6 +45,84 @@ class Conta_Bancaria():
     def deposito(self):
         self.saldo += int(input("quantidade para depositar: "))
 
+
+
+
+class EntradaCinema():
+    def __init__(self):
+        self.dia = 0
+        self.dia_semana = ""
+        self.hora = 0
+        self.preco = 0
+        self.entrada = ""
+
+    def calc_entrada(self):
+        self.dia = int(input("digite o dia do mês:\n"))
+        if self.dia > 31:
+            print("erro")
+        else:
+            self.dia_semana = int(input("escreva número do dia da semana:\n1- domingo\n2- segunda-feira\n3- terça-feira\n4- quarta-feira\n5- quinta-feira\n6- sexta-feira\n7- sábado\n"))
+            if self.dia_semana == 1:
+                self.dia_semana = "domingo"
+            elif self.dia_semana == 2:
+                self.dia_semana = "segunda-feira"
+            elif self.dia_semana == 3:
+                self.dia_semana = "terça-feira"
+            elif self.dia_semana == 4:
+                self.dia_semana = "quarta-feira"
+            elif self.dia_semana == 5:
+                self.dia_semana = "quinta-feira"
+            elif self.dia_semana == 6:
+                self.dia_semana = "sexta-feira"
+            elif self.dia_semana == 7:
+                self.dia_semana = "sábado"
+            self.hora = int(input("escreva o horário em formato hh:mm\n").split(":")[0])
+            if self.hora > 23:
+                print("erro no horário")
+            self.entrada = input("sua entrada é uma meia-entrada? s/n\n")
+            if self.entrada ==  "s":
+                self.entrada = "meia"
+            elif self.entrada == "n":
+                self.entrada = "normal"
+            else:
+                print("erro")
+
+            if self.dia_semana == "segunda-feira" or self.dia_semana == "terça-feira" or self.dia_semana == "quinta-feira":
+                self.preco = 16.00
+                if self.hora >= 17 and self.hora < 0:
+                    self.preco *= 1.5
+                if self.entrada == "meia":
+                    self.preco *= 0.5
+
+            elif self.dia_semana == "sexta-feira" or self.dia_semana == "sábado" or self.dia_semana == "domingo":
+                self.preco == 20.00
+                if self.hora >= 17 and self.hora < 0:
+                    self.preco *= 1.5
+                if self.entrada == "meia":
+                    self.preco *= 0.5
+
+            elif self.dia_semana == "quarta-feira":
+                self.preco = 8.00
+
+        print(f"o valor da entrada é {self.preco}R$")
+    
+
+
+
+if escolha == 1:
+    x = Circulo()
+    x.raio = int(input("raio: "))
+    x.calcular_area()
+    x.calcular_circunferencia()
+
+
+if escolha == 2:
+    y = Viagem()
+    y.km = int(input("km andados: "))
+    y.horas = input("tempo em formato hh:mm:\n")
+    y.calc_velocidade()
+
+
 if escolha == 3:
     z = Conta_Bancaria()
     z.titular =  "João Pedro"
@@ -65,64 +133,6 @@ if escolha == 3:
     z.deposito()
     print("saldo", z.saldo)
 
-
 if escolha == 4:
-    class EntradaCinema():
-        def __init__(self):
-            self.dia = 0
-            self.dia_semana = ""
-            self.hora = 0
-            self.preco = 0
-            self.entrada = ""
-
-        def calc_entrada(self):
-            self.dia = int(input("digite o dia do mês:\n"))
-            if self.dia > 31:
-                print("erro")
-            else:
-                self.dia_semana = int(input("escreva número do dia da semana:\n1- domingo\n2- segunda-feira\n3- terça-feira\n4- quarta-feira\n5- quinta-feira\n6- sexta-feira\n7- sábado\n"))
-                if self.dia_semana == 1:
-                    self.dia_semana = "domingo"
-                elif self.dia_semana == 2:
-                    self.dia_semana = "segunda-feira"
-                elif self.dia_semana == 3:
-                    self.dia_semana = "terça-feira"
-                elif self.dia_semana == 4:
-                    self.dia_semana = "quarta-feira"
-                elif self.dia_semana == 5:
-                    self.dia_semana = "quinta-feira"
-                elif self.dia_semana == 6:
-                    self.dia_semana = "sexta-feira"
-                elif self.dia_semana == 7:
-                    self.dia_semana = "sábado"
-                self.hora = int(input("escreva o horário em formato hh:mm\n").split(":")[0])
-                if self.hora > 23:
-                    print("erro no horário")
-                self.entrada = input("sua entrada é uma meia-entrada? s/n\n")
-                if self.entrada ==  "s":
-                    self.entrada = "meia"
-                elif self.entrada == "n":
-                    self.entrada = "normal"
-                else:
-                    print("erro")
-
-                if self.dia_semana == "segunda-feira" or self.dia_semana == "terça-feira" or self.dia_semana == "quinta-feira":
-                    self.preco = 16.00
-                    if self.hora >= 17 and self.hora < 0:
-                        self.preco *= 1.5
-                    if self.entrada == "meia":
-                        self.preco *= 0.5
-
-                elif self.dia_semana == "sexta-feira" or self.dia_semana == "sábado" or self.dia_semana == "domingo":
-                    self.preco == 20.00
-                    if self.hora >= 17 and self.hora < 0:
-                        self.preco *= 1.5
-                    if self.entrada == "meia":
-                        self.preco *= 0.5
-
-                elif self.dia_semana == "quarta-feira":
-                    self.preco = 8.00
-
-            print(f"o valor da entrada é {self.preco}R$")
     o = EntradaCinema()
     o.calc_entrada()
